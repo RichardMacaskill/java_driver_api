636b3791df7dd1259fe67898c8c168efd4927362
Richard Macaskill <richard.macaskill@neotechnology.com>
2023-05-15 17:17:46 +0100

    messy user switching

--- src/Driver_API.java
+++ src/Driver_API.java
@@ -1,6 +1,9 @@
 import org.neo4j.driver.*;
 
-import static org.neo4j.driver.AuthTokenManagers.expirationBased;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 import static org.neo4j.driver.GraphDatabase.driver;
 
@@ -8,26 +11,57 @@ public class Driver_API {
 
 
     public static void main(String[] args) {
-
         String uri = System.getenv("NEO4J_URI");
         String password = System.getenv("NEO4J_PWD");
 
-        String user = "moviesuser";
-        //var manager = AuthTokenManagers.expirationBased(() -> {
+        List<String> list = new ArrayList<String>();
+        list.add("moviesuser");
+        list.add("neo4j");
+        list.add("footballtransfersuser");
+        list.add("moviesuser");
+        
+        String[] user = list.toArray(new String[0]);
 
-            //return token.expiringAt(timestamp); // a new method on AuthToken introduced for the supplied expiration based AuthTokenManager implementation
-  //          return new AuthToken("").expiringAt(timestamp);
-//        });
-        try (Driver driver = driver(uri, AuthTokens.basic(user, password))) {
+        DoStuff(uri,user,password);
 
-            var results = driver.executableQuery("MATCH (m:Movie) return m;")
-                    .withConfig(QueryConfig.builder()
-                            .withRouting(RoutingControl.READ).build()).
-                    execute();
+    }
+    private static void DoStuff(String URI,String[] userNames, String Password)
+    {
 
-            System.out.println(new StringBuilder().append((long) results.records().size()).append(" records returned").toString());
+        var manager = AuthTokenManagers.expirationBased(() -> {
+            Date now = new Date(new Date().getTime());
+            Date expDate = new Date(new Date().getTime() +100); //java.util.Date
 
+            Random rand = new Random();
+            int randomNum = rand.nextInt(4);
+           // System.out.println(now.toString());
+            System.out.println(expDate.toString());
+
+            var token = AuthTokens.basic(userNames[randomNum], Password);
+            return token.expiringAt(expDate.getTime()); // a new method on AuthToken introduced for the supplied expiration based AuthTokenManager implementation
+        });
+
+
+        try (Driver driver = driver(URI, manager)) {
+
+            for(int i=0; i < 1000 ; i++) {
+                var results = driver.executableQuery("MATCH (m:Movie) return m;")
+                        .withConfig(QueryConfig.builder().withRouting(RoutingControl.READ).build())
+                        .execute();
+
+                System.out.println(new StringBuilder().append((long) results.records().size()).append(" records returned").toString());
+
+                try {
+                    Thread.sleep(101);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+
+            }
 
         }
     }
+
+
+
 }
